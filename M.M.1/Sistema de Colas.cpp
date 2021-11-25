c97e28207db3d00af1e6f438da31cc2a8710987e
Andres Ardila <andres.ardila.agudelo@gmail.com>
2021-11-25 15:24:07 -0500

    lcgrand

--- M.M.1/Sistema de Colas.cpp
+++ M.M.1/Sistema de Colas.cpp
@@ -253,16 +253,16 @@ void reportes(void) /* Funcion generadora de reportes. */
     fprintf(resultados, "Numero de clientes%14d\n\n", num_esperas_requerido);
 
     /* Calcula y estima los estimados de las medidas deseadas de desempe�o */
-    fprintf(resultados, "\n\nEspera promedio en la cola%11.3f minutos\n\n",
+    fprintf(resultados, "\n\nEspera promedio en la cola%11.20f minutos\n\n",
             total_de_esperas / num_clientes_espera);
-    fprintf(resultados, "Numero promedio en cola%10.3f\n\n",
+    fprintf(resultados, "Numero promedio en cola%10.20f\n\n",
             area_num_entra_cola / tiempo_simulacion);
     fprintf(resultados, "Uso del servidor%15.3f\n\n",
             area_estado_servidor / tiempo_simulacion);
     fprintf(resultados, "Tiempo de terminacion de la simulacion%12.3f minutos", tiempo_simulacion);
 }
 
-float expon(float media) /* Funcion generadora de la exponencias */
+float expon(float media) /* Funcion percentil exponencial */
 {
     /* Retorna una variable aleatoria exponencial con media "media"*/
 
